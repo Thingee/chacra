3b79d4866f77c11762a2b5cc72f377a31e9f9b7d
Mike Perez <thingee@gmail.com>
2024-09-26 13:11:44 -0700

    Update psycopg version
    Signed-off-by: Mike Perez <thingee@gmail.com>

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=[
         "pecan",
         "sqlalchemy==1.3.0",
-        "psycopg2",
+        "psycopg2-binary==2.9.9",
         "pecan-notario",
         "python-statsd",
         "requests",
